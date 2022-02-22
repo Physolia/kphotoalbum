1e0398b285ef821179965999494fbfa248c30e24
Jesper K. Pedersen <jesper.pedersen@kdab.com>
2022-02-22 10:34:11 +0100

    Print command type using EnumHelper.
    Thanks QWAM.
    https://www.youtube.com/watch?v=hWh_bA0-_KM&list=PL6CJYn40gN6gf-G-
    o6syFwGrtq3kItEqI&index=22

--- RemoteControl/RemoteConnection.cpp
+++ RemoteControl/RemoteConnection.cpp
@@ -7,6 +7,7 @@
 
 #include "RemoteCommand.h"
 
+#include "../Utilities/EnumHelper.h"
 #include <QApplication>
 #include <QBuffer>
 #include <QTcpSocket>
@@ -31,7 +32,7 @@ RemoteConnection::RemoteConnection(QObject *parent)
 void RemoteConnection::sendCommand(const RemoteCommand &command)
 {
     protocolDebug() << qPrintable(QTime::currentTime().toString(QString::fromUtf8("hh:mm:ss.zzz")))
-                    << ": Sending " << QString::number((int)command.commandType());
+                    << ": Sending " << EnumHelper::toString(command.commandType());
     Q_ASSERT(QThread::currentThread() == qApp->thread());
 
     if (!isConnected())
@@ -92,10 +93,11 @@ void RemoteConnection::dataReceived()
             qint32 id;
             stream >> id;
 
-            std::unique_ptr<RemoteCommand> command = RemoteCommand::create(static_cast<CommandType>(id));
+            auto commandType = static_cast<CommandType>(id);
+            std::unique_ptr<RemoteCommand> command = RemoteCommand::create(commandType);
             command->decode(stream);
             protocolDebug() << qPrintable(QTime::currentTime().toString(QString::fromUtf8("hh:mm:ss.zzz")))
-                            << ": Received " << id;
+                            << ": Received " << EnumHelper::toString(commandType);
 
             emit gotCommand(*command);
         }
